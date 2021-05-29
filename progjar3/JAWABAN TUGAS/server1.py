5efdce96c3f3b02794b5512050dfb963a054cee4
root <ridaadila10@gmail.com>
2021-05-29 05:03:25 +0000

    tes

--- progjar3/JAWABAN TUGAS/server1.py
+++ progjar3/JAWABAN TUGAS/server1.py
@@ -1,7 +1,7 @@
 import socket
 import sys
 
-IP_ADDRESS = '192.168.122.48'
+IP_ADDRESS = '192.168.122.234'
 PORT = 5005
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -10,14 +10,14 @@ sock.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST, 1)
 
 sock.bind(("", PORT))
 
-nama_file = ['hasil1.jpg','hasil2.jpg']
-count = 0
+#nama_file = ['hasil1.jpg','hasil2.jpg']
+count = 1
 while True:
     data, addr = sock.recvfrom(1024)
     print(addr)
     print("diterima ", data)
     print("dikirim oleh ", addr)
-    fp = open(nama_file[count], 'wb')
+    fp = open("hasil"+str(count)+".jpg", 'wb')
     fp.write(data)
     count+=1
 
