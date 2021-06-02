0643b253118c76ce549ee53651e4df03d26114b4
ridaadila <ridaadila10@gmail.com>
2021-06-02 14:02:09 +0700

    add file tugas

---
+++ progjar4a/Jawaban Tugas/file_client_cli.py
@@ -0,0 +1,91 @@
+import socket
+import json
+import base64
+import logging
+import threading
+from multiprocessing import Process
+import time
+import datetime
+
+server_address=('0.0.0.0',7777)
+
+def send_command(command_str=""):
+    global server_address
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(server_address)
+    logging.warning(f"connecting to {server_address}")
+    try:
+        logging.warning(f"sending message ")
+        sock.sendall(command_str.encode())
+        # Look for the response, waiting until socket is done (no more data)
+        data_received="" #empty string
+        while True:
+            #socket does not receive all data at once, data comes in part, need to be concatenated at the end of process
+            data = sock.recv(16)
+            if data:
+                #data is not empty, concat with previous content
+                data_received += data.decode()
+                if "\r\n\r\n" in data_received:
+                    break
+            else:
+                # no more data, stop the process by break
+                break
+        # at this point, data_received (string) will contain all data coming from the socket
+        # to be able to use the data_received as a dict, need to load it using json.loads()
+        hasil = json.loads(data_received)
+        logging.warning("data received from server:")
+        return hasil
+    except:
+        logging.warning("error during data receiving")
+        return False
+
+
+def remote_list():
+    command_str=f"LIST"
+    hasil = send_command(command_str)
+    if (hasil['status']=='OK'):
+        print("daftar file : ")
+        for nmfile in hasil['data']:
+            print(f"- {nmfile}")
+        return True
+    else:
+        print("Gagal")
+        return False
+
+def remote_get(filename=""):
+    command_str=f"GET {filename}"
+    hasil = send_command(command_str)
+    if (hasil['status']=='OK'):
+        #proses file dalam bentuk base64 ke bentuk bytes
+        namafile= hasil['data_namafile']
+        isifile = base64.b64decode(hasil['data_file'])
+        fp = open(namafile,'wb+')
+        fp.write(isifile)
+        fp.close()
+        return True
+    else:
+        print("Gagal")
+        return False
+
+def get_img(filename=""):
+    texec = dict()
+    namafile = filename
+    catat_awal = datetime.datetime.now()
+    for k in range(100):
+        print("mendownload get ", namafile, " ke- ", k)
+        waktu = time.time()
+        # bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multiprocess
+        texec[k] = Process(target=remote_get, args=(namafile,))
+        texec[k].start()
+    # setelah menyelesaikan tugasnya, dikembalikan ke main process dengan join
+    for k in range(100):
+        texec[k].join()
+    catat_akhir = datetime.datetime.now()
+    selesai = catat_akhir - catat_awal
+    print(f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}")
+
+if __name__=='__main__':
+    server_address=('0.0.0.0',6666)
+    remote_list()
+    get_img('pokijan.jpg')
+
